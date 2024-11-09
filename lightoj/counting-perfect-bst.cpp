c3f69548d612087376dc7d3f4df95e4aaba2101f
akagiyuu <khanghuynh0245@gmail.com>
2024-11-09 22:39:59 +0700

    feat(lightoj): counting-perfect-bst

--- lightoj/counting-perfect-bst.cpp
+++ lightoj/counting-perfect-bst.cpp
@@ -7,6 +7,19 @@ typedef long long ll;
 
 const ll MOD = 1e8 + 7;
 const ll N = 2e6;
+const ll MAX_VAL = 1e10;
+
+ll pow(ll a, ll b)
+{
+	ll res = 1;
+	while (b) {
+		if (b & 1)
+			res = res * a;
+		a = a * a;
+		b >>= 1;
+	}
+	return res;
+}
 
 ll powmod(ll a, ll b)
 {
@@ -21,7 +34,9 @@ ll powmod(ll a, ll b)
 }
 
 ll fact[N + 1], ifact[N + 1];
-void build()
+vector<ll> perfect_numbers;
+
+vector<ll> build()
 {
 	fact[0] = 1;
 	for (ll i = 1; i <= N; i++) {
@@ -31,6 +46,24 @@ void build()
 	for (ll i = N - 1; i >= 0; i--) {
 		ifact[i] = ifact[i + 1] * (i + 1) % MOD;
 	}
+
+    set<ll> tmp;
+	for (ll base = 2; base <= (ll)1e5; base++) {
+		ll cur = base * base;
+		while (cur <= MAX_VAL) {
+			tmp.insert(cur);
+			cur *= base;
+		}
+	}
+    vector<ll> nums(tmp.begin(), tmp.end());
+    return nums;
+}
+
+ll count_perfect(ll a, ll b)
+{
+	ll left = lower_bound(perfect_numbers.begin(), perfect_numbers.end(), a) - perfect_numbers.begin();
+	ll right = upper_bound(perfect_numbers.begin(), perfect_numbers.end(), b) - perfect_numbers.begin() - 1;
+    return right - left + 1;
 }
 
 ll catalan(ll n)
@@ -38,39 +71,11 @@ ll catalan(ll n)
 	return fact[2 * n] * ifact[n] % MOD * ifact[n + 1] % MOD;
 }
 
-bool is_perfect(ll n)
-{
-	ll count = -1;
-	for (ll i = 2; i * i <= n; i++) {
-		if (n % i != 0)
-			continue;
-		ll c = 0;
-		while (n % i == 0) {
-			c++;
-			n /= i;
-		}
-		if (count == -1) {
-			count = c;
-			continue;
-		}
-		if (count != c)
-			return false;
-	}
-	if (n > 1) {
-		return false;
-	}
-	return count > 1;
-}
-
 void solve()
 {
 	ll a, b;
 	cin >> a >> b;
-	ll count = 0;
-	for (ll i = a; i <= b; i++) {
-		if (is_perfect(i))
-			count++;
-	}
+	ll count = count_perfect(a, b);
 	ll res = count == 0 ? 0 : catalan(count);
 	cout << res << "\n";
 }
@@ -82,7 +87,7 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	build();
+	perfect_numbers = build();
 	int t;
 	cin >> t;
 	for (ll i = 1; i <= t; i++) {

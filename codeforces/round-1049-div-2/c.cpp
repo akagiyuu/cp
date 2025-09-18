12db0262db1da97b04ebab712b1485a49ab5d344
akagiyuu <khanghuynh0245@gmail.com>
2025-09-18 10:09:12 +0700

    update

--- codeforces/round-1049-div-2/c.cpp
+++ codeforces/round-1049-div-2/c.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <cpp-dump.hpp>
 
 using namespace std;
 
@@ -10,7 +9,6 @@ const ll MOD = 1000000007;
 void solve()
 {
 	ll n, score = 0;
-	short turn = 0;
 	cin >> n;
 	vector<ll> a(n);
 	for (ll i = 0; i < n; i++) {
@@ -20,78 +18,32 @@ void solve()
 		else
 			score -= a[i];
 	}
-	if (n == 1) {
-		cout << a[0] << "\n";
-		return;
+
+	// even l
+	ll prefix = LONG_LONG_MIN;
+	ll max_diff = (n - 1) / 2 * 2;
+	ll l = n % 2 == 0 ? n - 2 : n - 3;
+	for (; l >= 0; l -= 2) {
+		ll val_l = -2 * a[l] - l;
+		prefix = max(prefix, 2 * a[l + 1] + (l + 1));
+		max_diff = max(max_diff, prefix + val_l);
 	}
 
-	auto cal_l = [a](ll l) {
-		if (l % 2 == 0)
-			return 2 * a[l] - l;
-		else
-			return -2 * a[l] - l;
-	};
-	auto cal_r = [a](ll r) {
-		if (r % 2 == 0)
-			return -2 * a[r] + r;
-		else
-			return 2 * a[r] + r;
-	};
-
-	for (ll round = 0; round < 5; round++) {
-		short turn = round % 2;
-		ll choose_l = n - 2, choose_r = n - 1;
-		ll suffix_r = n - 1;
-		ll suffix = cal_r(suffix_r);
-
-		for (ll l = n - 2; l >= 0; l--) {
-			ll left = cal_l(l);
-			if (turn == 0) {
-				if (cal_r(l + 1) > suffix) {
-					suffix = cal_r(l + 1);
-					suffix_r = l + 1;
-				}
-
-				if (left + suffix > cal_l(choose_l) + cal_r(choose_r)) {
-					choose_l = l;
-					choose_r = suffix_r;
-				}
-			} else {
-				if (cal_r(l + 1) < suffix) {
-					suffix = cal_r(l + 1);
-					suffix_r = l+ 1;
-				}
-
-				if (left + suffix < cal_l(choose_l) + cal_r(choose_r)) {
-					choose_l = l;
-					choose_r = suffix_r;
-				}
-			}
-		}
-
-		ll cur = score + cal_l(choose_l) + cal_r(choose_r);
-        cpp_dump(turn);
-        cpp_dump(choose_l, choose_r);
-        cpp_dump(cur);
-		if (turn == 0) {
-			if (cur <= score) {
-				break;
-			}
-		} else {
-			if (cur >= score) {
-				break;
-			}
-		}
-
-		score = cur;
-		tie(a[choose_l], a[choose_r]) = make_pair(a[choose_r], a[choose_l]);
+	// odd l
+	prefix = LONG_LONG_MIN;
+	l = n % 2 == 0 ? n - 3 : n - 2;
+	for (; l >= 0; l -= 2) {
+		ll val_l = 2 * a[l] - l;
+		prefix = max(prefix, l + 1 - 2 * a[l + 1]);
+		max_diff = max(max_diff, prefix + val_l);
 	}
-    cout << score << "\n";
+
+	score = max(score, score + max_diff);
+	cout << score << "\n";
 }
 
 int main()
 {
-	CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);

1a4ba870dc7c10b88cf3c773933accf7926dcde9
akagiyuu <khanghuynh0245@gmail.com>
2025-11-04 23:05:30 +0700

    update

--- hackerrank/chessboard-game-again.cpp
+++ hackerrank/chessboard-game-again.cpp
@@ -1,72 +1,65 @@
 #include <bits/stdc++.h>
-// #include <cpp-dump.hpp>
+
+#define int long long
 
 using namespace std;
+const int N = 16;
+int g[N][N];
 
-typedef long long ll;
-
-const ll MOD = 1000000007;
-
-int mex(set<ll> const &a)
+int grundy(int i, int j)
 {
-	ll result = 0;
-	while (a.count(result))
-		++result;
-	return result;
+	if (g[i][j] != -1)
+		return g[i][j];
+	unordered_set<int> s;
+	if (i - 2 >= 1 && j + 1 <= 15) {
+		s.insert(grundy(i - 2, j + 1));
+	}
+	if (i - 2 >= 1 && j - 1 >= 1) {
+		s.insert(grundy(i - 2, j - 1));
+	}
+	if (i + 1 <= 15 && j - 2 >= 1) {
+		s.insert(grundy(i + 1, j - 2));
+	}
+	if (i - 1 >= 1 && j - 2 >= 1) {
+		s.insert(grundy(i - 1, j - 2));
+	}
+	int mex = 0;
+	while (s.find(mex) != s.end())
+		mex++;
+	g[i][j] = mex;
+	return mex;
 }
 
-template <> struct std::hash<pair<ll, ll> > {
-	size_t operator()(const pair<ll, ll> &a) const
-	{
-		auto f = hash<ll>();
-		return f(a.first) ^ f(a.second);
-	}
-};
-
-ll g(pair<ll, ll> key, unordered_map<pair<ll, ll>, ll> &cache)
+bool solve()
 {
-	if (cache.find(key) != cache.end())
-		return cache[key];
-	set<ll> state;
-	for (auto [dx, dy] : { make_pair(-2, 1), make_pair(-2, -1), make_pair(1, -2), make_pair(-1, -2) }) {
-		ll next_x = key.first + dx;
-		ll next_y = key.second + dy;
-		if (next_x < 1 || next_x > 15 || next_y < 1 || next_y > 15)
-			continue;
-        state.insert(g(make_pair(next_x, next_y), cache));
-	}
-	ll res = mex(state);
-	cache[key] = res;
-	return res;
-}
-
-unordered_map<pair<ll, ll>, ll> cache;
-
-void solve()
-{
-	ll n;
+	int n;
 	cin >> n;
-	ll res = 0;
-	pair<ll, ll> x;
-	for (ll i = 0; i < n; i++) {
-		cin >> x.first >> x.second;
-		res ^= g(x, cache);
+	int res = 0, i, j;
+	while (n--) {
+		cin >> i >> j;
+		res ^= grundy(i, j);
 	}
-	if (res > 0)
-		cout << "First\n";
-	else
-		cout << "Second\n";
+	return res != 0;
 }
 
-int main()
+signed main()
 {
-	// CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			g[i][j] = -1;
+		}
+	}
+
 	int t;
 	cin >> t;
-	while (t--)
-		solve();
+	while (t--) {
+		if (solve())
+			cout << "First\n";
+		else
+			cout << "Second\n";
+	}
 }

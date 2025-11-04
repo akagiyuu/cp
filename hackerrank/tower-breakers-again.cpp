585162668f73890541927ed311b0d04c827c1677
akagiyuu <khanghuynh0245@gmail.com>
2025-11-04 22:50:59 +0700

    update

--- hackerrank/tower-breakers-again.cpp
+++ hackerrank/tower-breakers-again.cpp
@@ -1,79 +1,70 @@
 #include <bits/stdc++.h>
-// #include <cpp-dump.hpp>
+
+#define int long long
 
 using namespace std;
 
-typedef long long ll;
+const int N = 1e5 + 1;
+int g[N];
 
-const ll MOD = 1000000007;
-const ll N = 1e6;
-
-vector<ll> divisors[N + 1];
-
-void build()
+int sum_pw(int n)
 {
-	for (ll i = 2; i <= N; i++) {
-		for (ll j = i; j <= N; j += i) {
-			divisors[j].push_back(i);
+	int res = 0;
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			n /= i;
+			res++;
 		}
 	}
-}
+	if (n > 1)
+		res++;
 
-int mex(set<ll> const &a)
-{
-	int result = 0;
-	while (a.count(result))
-		++result;
-	return result;
-}
-
-ll cal_grunt(ll key, unordered_map<ll, ll> &cache)
-{
-	if (cache.find(key) != cache.end()) {
-		return cache[key];
-	}
-	if (divisors[key].size() == 0) {
-		return 0;
-	}
-	set<ll> state;
-	for (auto d : divisors[key]) {
-		if (d % 2 == 0)
-			state.insert(0);
-		else
-			state.insert(cal_grunt(key / d, cache));
-	}
-	ll res = mex(state);
-	cache[key] = res;
 	return res;
 }
 
-void solve()
+void build()
 {
-	ll n, x;
-	cin >> n;
-	ll res = 0;
-	for (ll i = 0; i < n; i++) {
-		cin >> x;
-		unordered_map<ll, ll> cache;
-		res ^= cal_grunt(x, cache);
-	}
-	if (res > 0) {
-		cout << "1\n";
-	} else {
-		cout << "2\n";
+	g[1] = 0;
+	for (int i = 2; i < N; i++) {
+		int j = i;
+		int cnt = 0;
+		while (j % 2 == 0) {
+			cnt++;
+			j /= 2;
+		}
+		g[i] = sum_pw(j);
+		if (cnt >= 1)
+			g[i]++;
 	}
 }
 
-int main()
+bool solve()
 {
-	// CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
+	int n;
+	cin >> n;
+
+	int res = 0, tmp;
+	while (n--) {
+		cin >> tmp;
+		res ^= g[tmp];
+	}
+	return res != 0;
+}
+
+signed main()
+{
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	build();
 
 	int t;
 	cin >> t;
-	while (t--)
-		solve();
+	while (t--) {
+		if (solve())
+			cout << "1\n";
+		else
+			cout << "2\n";
+	}
 }

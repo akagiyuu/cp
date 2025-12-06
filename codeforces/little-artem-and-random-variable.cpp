a250e27512fc6e1e4e2da9dee636750a79c71374
akagiyuu <khanghuynh0245@gmail.com>
2025-12-06 22:59:33 +0700

    update

--- codeforces/little-artem-and-random-variable.cpp
+++ codeforces/little-artem-and-random-variable.cpp
@@ -1,82 +1,82 @@
 #include <bits/stdc++.h>
+#include <cmath>
 
 #define int long long
 #define double long double
+#define fi first
+#define se second
 
 using namespace std;
 
-const double INF = 1e18;
-const double EPS = 1e-6;
+const double EPS = 1e-8;
 
-pair<double, double> find_root(double a, double b, double c)
-{
-	double delta = b * b - 4 * a * c;
-	if (delta < -EPS)
-		return { -INF, -INF };
-	double rt_delta = sqrtl(delta);
-	return {
-		(-b + rt_delta) / (2.L * a),
-		(-b - rt_delta) / (2.L * a),
-	};
-}
-
-vector<double> M, m, s, suffix, p;
-
-bool backtrack(int i, double s_prob)
-{
-	if (i == -1) {
-		return fabsl(s_prob - 1) < EPS;
-	}
-	auto root = find_root(-1, suffix[i + 1] - 2 * s_prob + s[i], s_prob * s[i] - m[i]);
-	if (root.first >= -EPS && s[i] - root.first >= -EPS) {
-		p[i] = root.first;
-		if (backtrack(i - 1, s_prob + p[i]))
-			return true;
-	}
-	if (root.second >= -EPS && s[i] - root.second >= -EPS) {
-		p[i] = root.second;
-		if (backtrack(i - 1, s_prob + p[i]))
-			return true;
-	}
-
-	return false;
-}
+// pair<double, double> find_root(double a, double b, double c)
+// {
+// 	double delta = b * b - 4 * a * c;
+// 	double rt_delta = sqrtl(delta);
+// 	return {
+// 		(-b - rt_delta) / (2.L * a),
+// 		(-b + rt_delta) / (2.L * a),
+// 	};
+// }
+// bool backtrack(int i, vector<pair<double, double> > &res)
+// {
+// 	int n = res.size();
+// 	if (i == n)
+// 		return true;
+// 	if (res[i].fi - res[i - 1].fi >= -EPS && res[i].se - res[i - 1].se >= -EPS) {
+// 		if (backtrack(i + 1, res))
+// 			return true;
+// 	}
+// 	swap(res[i].fi, res[i].se);
+// 	if (res[i].fi - res[i - 1].fi >= -EPS && res[i].se - res[i - 1].se >= -EPS) {
+// 		if (backtrack(i + 1, res))
+// 			return true;
+// 	}
+// 	return false;
+// }
 
 void solve()
 {
 	int n;
 	cin >> n;
-	M.resize(n);
-	m.resize(n);
-	s.resize(n);
-	suffix.resize(n);
-	p.resize(n);
+	vector<double> M(n), m(n);
 	for (int i = 0; i < n; i++) {
 		cin >> M[i];
 	}
 	for (int i = 0; i < n; i++) {
 		cin >> m[i];
 	}
+	for (int i = 1; i < n; i++) {
+		m[i] += m[i - 1];
+		M[i] += M[i - 1];
+	}
+	// for (int i = 0; i < n; i++) {
+	// 	cout << sum[i] << " " << product[i] << "\n";
+	// }
+
+	vector<pair<double, double> > res(n);
 	for (int i = 0; i < n; i++) {
-		s[i] = M[i] + m[i];
+		double x = m[i];
+		double y = M[i];
+		double delta = fabs((x + y) * (x + y) - 4 * y);
+		res[i] = {
+			(x + y + sqrtl(delta)) / 2.0L,
+			(x + y - sqrtl(delta)) / 2.0L,
+		};
 	}
-	suffix[n - 1] = s[n - 1];
-	for (int i = n - 2; i >= 0; i--) {
-		suffix[i] = suffix[i + 1] + s[i];
+	// backtrack(1, res);
+	for (int i = n - 1; i > 0; i--) {
+		res[i].fi -= res[i - 1].fi;
+		res[i].se -= res[i - 1].se;
 	}
-	auto root = find_root(1.L, -s[n - 1], m[n - 1]);
-	p[n - 1] = root.first;
-	if(!backtrack(n - 2, p[n - 1])) {
-		p[n - 1] = root.second;
-		backtrack(n - 2, p[n - 1]);
-	};
 
 	for (int i = 0; i < n; i++) {
-		cout << setprecision(10) << fabsl(p[i]) << " ";
+		cout << fixed << setprecision(10) << res[i].fi << " ";
 	}
 	cout << "\n";
 	for (int i = 0; i < n; i++) {
-		cout << setprecision(10) << fabsl(s[i] - p[i]) << " ";
+		cout << fixed << setprecision(10) << res[i].se << " ";
 	}
 	cout << "\n";
 }

999160d7b8f39597e0950958b6537215af787d03
akagiyuu <khanghuynh0245@gmail.com>
2026-06-24 07:42:54 +0700

    update

--- codeforces/homecoming.cpp
+++ codeforces/homecoming.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <cpp-dump.hpp>
 
 using namespace std;
 
@@ -17,25 +16,36 @@ void solve()
 	ll n = s.size();
 	ll res = n - 1;
 	vector<ll> price(n, 0);
-	if(s[n - 2] == s[n - 1] && s[n - 2] == 'B') price[n - 2] = a;
-	if(s[n - 2] == s[n - 1] && s[n - 2] == 'A') price[n - 2] = b;
-
-	for (ll i = n - 3; i >= 0; i--) {
-		price[i] = price[i + 1];
-		if (s[i] != s[i + 1] && s[i] == 'B')
-			price[i] += a;
-		if (s[i] != s[i + 1] && s[i] == 'A')
-			price[i] += b;
-		if(price[i] > p) break;
+	int i = n - 2;
+	while (i >= 0 && s[i] == s[n - 1]) {
+		if (s[i] == 'A')
+			price[i] = a;
+		if (s[i] == 'B')
+			price[i] = b;
+		if (price[i] > p) {
+			i--;
+			break;
+		}
 		res = i;
+		i--;
 	}
-	cpp_dump(price);
+	while (i >= 0) {
+		price[i] = price[i + 1];
+		if (s[i] != s[i + 1] && s[i] == 'A')
+			price[i] += a;
+		if (s[i] != s[i + 1] && s[i] == 'B')
+			price[i] += b;
+		if (price[i] > p)
+			break;
+		res = i;
+		i--;
+	}
+
 	cout << res + 1 << "\n";
 }
 
 int main()
 {
-	CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);

83ef5021eea670114205cb6145eae2b2e74c16be
akagiyuu <khanghuynh0245@gmail.com>
2026-06-24 07:30:51 +0700

    update

--- codeforces/balanced-removals.cpp
+++ codeforces/balanced-removals.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include <cassert>
+#include <cpp-dump.hpp>
 
 using namespace std;
 
@@ -8,13 +8,16 @@ typedef array<ll, 3> pt;
 
 const ll MOD = 1000000007;
 
-ll sq_dist(const pt &a, const pt &b)
+ll remove_x(ll l, ll r, const vector<pt> &a)
 {
-	ll dx = a[0] - b[0];
-	ll dy = a[1] - b[1];
-	ll dz = a[2] - b[2];
+}
 
-	return dx * dx + dy * dy + dz * dz;
+ll remove_y(ll l, ll r, const vector<pt> &a)
+{
+}
+
+ll remove_z(ll l, ll r, const vector<pt> &a)
+{
 }
 
 void solve()
@@ -23,32 +26,14 @@ void solve()
 	cin >> n;
 	vector<pt> a(n);
 	for (ll i = 0; i < n; i++) {
-		cin >> a[i][0] >> a[i][1] >> a[i][2];
+		cin >> a[i][2] >> a[i][1] >> a[i][0];
 	}
-
-	vector<bool> is_removed(n, false);
-	for (ll i = 0; i < n; i++) {
-		if (is_removed[i])
-			continue;
-		ll min_j = -1;
-		for (ll j = i + 1; j < n; j++) {
-			if (is_removed[j])
-				continue;
-			if (min_j == -1)
-				min_j = j;
-			else if (sq_dist(a[i], a[j]) < sq_dist(a[i], a[min_j])) {
-				min_j = j;
-			}
-		}
-		assert(!is_removed[min_j]);
-		cout << i + 1 << " " << min_j + 1 << "\n";
-		is_removed[i] = true;
-		is_removed[min_j] = true;
-	}
+	sort(a.begin(), a.end());
 }
 
 int main()
 {
+	CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
---
+++ codeforces/soldier-and-number-game.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const ll MOD = 1000000007;
+const ll N = 1e6 + 7;
+
+ll lp[N], sum[N];
+
+void build()
+{
+	vector<ll> pr;
+	for (ll i = 2; i < N; i++) {
+		if (lp[i] == 0) {
+			lp[i] = i;
+			pr.push_back(i);
+		}
+		for (ll j = 0; i * pr[j] < N; j++) {
+			lp[i * pr[j]] = pr[j];
+			if (pr[j] == lp[i])
+				break;
+		}
+	}
+
+	sum[1] = 0;
+	for (ll i = 2; i < N; i++) {
+		sum[i] = sum[i / lp[i]] + 1;
+	}
+	for (ll i = 2; i < N; i++) {
+		sum[i] += sum[i - 1];
+	}
+}
+
+void solve()
+{
+	ll a, b;
+	cin >> a >> b;
+	cout << sum[a] - sum[b] << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	build();
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+}

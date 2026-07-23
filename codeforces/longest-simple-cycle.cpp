fce78e350822c63efaaf5b917e42bb1e6264ae01
akagiyuu <khanghuynh0245@gmail.com>
2026-07-23 21:20:10 +0700

    feat(codeforces): 1476C - Longest Simple Cycle

--- codeforces/longest-simple-cycle.cpp
+++ codeforces/longest-simple-cycle.cpp
@@ -10,29 +10,39 @@ void solve()
 {
 	int n;
 	cin >> n;
-	vector<int> c(n), a(n), b(n);
+	vector<int> c(n), a(n - 1), b(n - 1);
 	for (int i = 0; i < n; i++)
 		cin >> c[i];
 	int _tmp;
 	cin >> _tmp;
 	for (int i = 0; i < n - 1; i++)
 		cin >> a[i];
-	a[n - 1] = 1;
 	cin >> _tmp;
 	for (int i = 0; i < n - 1; i++)
 		cin >> b[i];
-	b[n - 1] = n;
 
 	vector<int> dp(n);
-	dp[n - 2] = abs(a[n - 1] - b[n - 2]) + abs(a[n - 2] - b[n - 2]) + 2;
+	dp[n - 2] = c[n - 1] - 1 + abs(a[n - 2] - b[n - 2]) + 2;
+	// cout << dp[n - 2] << " ";
 	for (int i = n - 3; i >= 0; i--) {
-		if (i == 0 || a[i + 1] == b[i + 1]) {
-			dp[i] = c[i + 1] - 1
-		} else {
-			cur += x - 1;
-			cur += c[i] - y;
-		}
+		int last = abs(a[i] - b[i]) + 2;
+
+		dp[i] = c[i + 1] - 1 + last;
+		// cout << dp[i] << " ";
+		if (a[i + 1] == b[i + 1])
+			continue;
+
+		dp[i] = max(dp[i], dp[i + 1] - abs(a[i + 1] - b[i + 1]) + min(a[i + 1], b[i + 1]) - 1 + c[i + 1] -
+					   max(a[i + 1], b[i + 1]) + last);
 	}
+	// cout << "\n";
+	int res = 0;
+	for (int i = n - 2; i >= 0; i--) {
+		// cout << dp[i] << " ";
+		res = max(res, dp[i]);
+	}
+	// cout << "\n";
+	cout << res << "\n";
 }
 
 signed main()

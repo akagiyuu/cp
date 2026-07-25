de2176847cb512ddcdde160320240fa2546ce45e
akagiyuu <khanghuynh0245@gmail.com>
2026-07-25 08:54:10 +0700

    feat(codeforces): 2244B - Nikita and Books

---
+++ codeforces/nikita-and-books.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+const int MOD = 1000000007;
+
+bool solve()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	int buffer = 0;
+	for (int i = 0; i < n; i++) {
+		a[i] += buffer;
+		if (a[i] < i + 1)
+			return false;
+		buffer = a[i] - (i + 1);
+	}
+
+	return true;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		if (solve())
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+}
